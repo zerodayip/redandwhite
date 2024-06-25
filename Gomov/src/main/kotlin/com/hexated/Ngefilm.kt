77d2e0d2ed3b53bbf5b782288136a1ae0600730c
ghost <ghost@gmail.com>
2024-06-25 12:57:40 +0700

    .

--- Gomov/src/main/kotlin/com/hexated/Ngefilm.kt
+++ Gomov/src/main/kotlin/com/hexated/Ngefilm.kt
@@ -4,7 +4,7 @@ import com.lagradost.cloudstream3.mainPageOf
 
 class Ngefilm : Gomov() {
 
-    override var mainUrl = "https://ngefilm21.pics"
+    override var mainUrl = "https://new10.ngefilm21.yachts"
 
     override var name = "Ngefilm"
     override val mainPage = mainPageOf(
