4f64eb776a65b6e6a8decbd94f85bcc950b128f4
Sofie <sofie.vivick99@gmail.com>
2025-09-18 03:01:41 +0700

    update

--- StremioX/build.gradle.kts
+++ StremioX/build.gradle.kts
@@ -3,9 +3,10 @@ import org.jetbrains.kotlin.konan.properties.Properties
 // use an integer for version numbers
 version = 13
 
-android.buildFeatures.buildConfig=true
-
 android {
+    buildFeatures {
+        buildConfig = true
+    }
     defaultConfig {
         val properties = Properties()
         properties.load(project.rootProject.file("local.properties").inputStream())
