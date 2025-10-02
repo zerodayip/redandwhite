2556e8e9d112e9d32e6e661ab0345bc2f34c2b33
zerodayip <duymaz.mrt@gmail.com>
2025-10-02 07:27:00 +0300

    Delete DramaidProvider directory

--- DramaidProvider/src/main/kotlin/com/hexated/Oppadrama.kt
+++
@@ -1,8 +0,0 @@
-package com.hexated
-
-import com.lagradost.cloudstream3.extractors.Filesim
-
-class Oppadrama : DramaidProvider() {
-    override var mainUrl = "http://176.97.113.111"
-    override var name = "Oppadrama"
-}
